1ee36c8f242d0f35c4b83dc0037e58b5a6b48b0d
nazaninnasab <dranoer@gmail.com>
2023-07-10 08:05:40 +0200

    Refactor: Encapsulate intersection method, simplify user input

--- app/src/main/java/com/dranoer/codingchallenges/main.kt
+++ app/src/main/java/com/dranoer/codingchallenges/main.kt
@@ -2,33 +2,22 @@ package com.dranoer.codingchallenges
 
 fun main() {
 
-    print("Enter Rectangle A info: ")
-    val ax1 = readLine()?.toIntOrNull()
-    val ax2 = readLine()?.toIntOrNull()
-    val ay1 = readLine()?.toIntOrNull()
-    val ay2 = readLine()?.toIntOrNull()
-
-    print("Enter Rectangle B info: ")
-    val bx1 = readLine()?.toIntOrNull()
-    val bx2 = readLine()?.toIntOrNull()
-    val by1 = readLine()?.toIntOrNull()
-    val by2 = readLine()?.toIntOrNull()
-
-
-    val result = recIntersect(
-        A = Rectangle(ax1!!, ax2!!, ay1!!, ay2!!),
-        B = Rectangle(bx1!!, bx2!!, by1!!, by2!!)
-    )
+    val rectA = promptForRectangle("A")
+    val rectB = promptForRectangle("B")
+    val result = rectA.intersects(rectB)
 
     println("This is $result that rectangle A & B have intersect")
 }
 
-data class Rectangle(val x1: Int, val x2: Int, val y1: Int, val y2: Int)
+data class Rectangle(val x1: Int, val x2: Int, val y1: Int, val y2: Int) {
+    fun intersects(other: Rectangle): Boolean {
 
-fun recIntersect(
-    A: Rectangle,
-    B: Rectangle
-): Boolean {
+        return (x1 < other.x2 && other.x1 < x2 && y1 < other.y2 && other.y1 < y2)
+    }
+}
 
-    return (A.x1 < B.x2 && B.x1 < A.x2 && A.y1 < B.y2 && B.y1 < A.y2)
+private fun promptForRectangle(label: String): Rectangle {
+    print("Enter $label rectangle info (format: x1, x2, y1, y2)")
+    val (x1, x2, y1, y2) = readLine()!!.split(' ').map(String::toInt)
+    return Rectangle(x1 = x1, x2 = x2, y1 = y1, y2 = y2)
 }
