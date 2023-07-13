aecb11390ae9582227dcb4b636731c6dcd37d18b
nazaninnasab <dranoer@gmail.com>
2023-07-13 20:40:07 +0200

    Refactor minor

--- app/src/main/java/com/dranoer/codingchallenges/array/TwoSum.kt
+++ app/src/main/java/com/dranoer/codingchallenges/array/TwoSum.kt
@@ -8,11 +8,11 @@ package com.dranoer.codingchallenges.array
         Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  */
 
-
 fun main() {
 
     val indexes = twoSum(nums = intArrayOf(2, 7, 11, 15), target = 9)
-    println("$indexes")
+    val prettyIndexes = indexes.joinToString(prefix = "[", postfix = "]")
+    println("Output: $prettyIndexes")
 }
 
 fun twoSum(nums: IntArray, target: Int): IntArray {
@@ -22,12 +22,12 @@ fun twoSum(nums: IntArray, target: Int): IntArray {
     for ((index, num) in nums.withIndex()) {
         val complement = target - num
 
-        if (map.containsKey(complement)) {
-            return intArrayOf(map[complement]!!, index)
+        map[complement]?.let {
+            return intArrayOf(it, index)
         }
 
         map[num] = index // Storing each number and its index in the map as it iterates through the array.
     }
 
-    throw IllegalArgumentException("No two sum solution")
+    throw IllegalArgumentException("No two sum solution for array: ${nums.contentToString()} and target: $target")
 }
