01b86a2a9dfb9173db0524883c6167552814fc8a
nazaninnasab <dranoer@gmail.com>
2023-07-13 20:31:15 +0200

    Add sum of two arrays

---
+++ app/src/main/java/com/dranoer/codingchallenges/array/TwoSum.kt
@@ -0,0 +1,33 @@
+package com.dranoer.codingchallenges.array
+
+/*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    Example:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+
+
+fun main() {
+
+    val indexes = twoSum(nums = intArrayOf(2, 7, 11, 15), target = 9)
+    println("$indexes")
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+
+    val map = HashMap<Int, Int>() // Number we process already and their indices
+
+    for ((index, num) in nums.withIndex()) {
+        val complement = target - num
+
+        if (map.containsKey(complement)) {
+            return intArrayOf(map[complement]!!, index)
+        }
+
+        map[num] = index // Storing each number and its index in the map as it iterates through the array.
+    }
+
+    throw IllegalArgumentException("No two sum solution")
+}
