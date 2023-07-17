3b42fc5187ed281bfd2203fcce7de753fef8f067
nazaninnasab <dranoer@gmail.com>
2023-07-17 23:48:03 +0200

    Add doc

--- app/src/main/java/com/dranoer/codingchallenges/array/RemoveDuplicates.kt
+++ app/src/main/java/com/dranoer/codingchallenges/array/RemoveDuplicates.kt
@@ -1,5 +1,11 @@
 package com.dranoer.codingchallenges.array
 
+/**
+ * Given an integer array nums sorted in non-decreasing order,
+ * remove the duplicates in-place such that each unique element appears only once.
+ * The relative order of the elements should be kept the same.
+ * Then return the number of unique elements in nums.
+ */
 
 fun main() {
     removeDuplicates(nums = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4))
